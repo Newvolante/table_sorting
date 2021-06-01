87e9f620d65a513d857cad5bd36f8bc723041c3e
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-01 15:59:17 +0200

    saving rows of the column into an array

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(function() {
     console.log('Script linked correctly to web page');
 
+    // an array of the rows present in the table
+    let $arrayOfRows = $('tbody').find('tr').toArray();
+    console.log($arrayOfRows);
+
     $('th').on('click', function() {
         console.log('clicked on the "' + $(this).data('sort') + '" header');
     });
