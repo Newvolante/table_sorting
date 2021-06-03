b24138720ea8afae92714be2c25186b37c9876b6
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-03 13:07:19 +0200

    added logic to reverse column sorting, if column already sorted

--- script.js
+++ script.js
@@ -7,21 +7,27 @@ $(function() {
 
     // click event on the column header
     $('tr').on('click', 'th', function() {
-        $headerName = $(this).data('sort');     // function name to call
-        $columnIndex = $('th').index(this);     // column number
+
+        $(this).siblings().removeClass('sorted');
+
+        if($(this).hasClass('sorted')) {
+            rows.reverse();
+        }
         
-        console.log(`clicked on column ${$columnIndex}`);
-
         // calling sorting function from sortObject.js
-        rows.sort(function(a, b) {
-            a = $(a).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
-            b = $(b).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
+        if(!$(this).hasClass('sorted')) {
+            $headerName = $(this).data('sort');     // function name to call
+            $columnIndex = $('th').index(this);     // column number
 
-            return sortObject[$headerName](a, b);
-        });
-
-        // appends the sorted arrays of rows to the table body
-        $('tbody').append(rows);
-
+            rows.sort(function(a, b) {
+                a = $(a).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
+                b = $(b).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
+                
+                return sortObject[$headerName](a, b);
+            });        
+            
+            $(this).addClass('sorted');
+        }        
+        $('tbody').append(rows);    // appends the sorted arrays of rows to the table body
     })
 });
