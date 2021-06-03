7f469b58b96ce4b178053cc69818752422239c4f
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-03 12:23:44 +0200

    optimized spaces in sortObject.js

--- sortObject.js
+++ sortObject.js
@@ -1,7 +1,5 @@
 let sortObject = {
-    name: function(a, b) {
-        
-
+    name: function (a, b) {
         if (a < b) {
             return -1;
         } else {
@@ -9,17 +7,17 @@ let sortObject = {
         }
     },
     duration: function (a, b) {
-        let tempA = a.split(':');   // holds mins and secs ad strings
-        let tempB = b.split(':');   // holds mins and secs ad strings
-        a = Number(tempA[0] * 60) + Number(tempA[1]);   // total of seconds
-        b = Number(tempB[0] * 60) + Number(tempB[1]);   // total of seconds
+        let tempA = a.split(':'); // holds mins and secs ad strings
+        let tempB = b.split(':'); // holds mins and secs ad strings
+        a = Number(tempA[0] * 60) + Number(tempA[1]); // total of seconds
+        b = Number(tempB[0] * 60) + Number(tempB[1]); // total of seconds
 
         return a - b;
     },
-    date: function(a, b) {
+    date: function (a, b) {
         a = new Date(a);
         b = new Date(b);
-        
+
         return a - b;
     }
 }
