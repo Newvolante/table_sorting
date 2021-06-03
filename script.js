fd665dad7754c7ce9388bbe1a9cc4c1520ede221
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-03 10:45:30 +0200

    added a function to sort rows based on the first column

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ $(function() {
     console.log('Script linked correctly to web page');
 
     // an array of the rows present in the table
-    let $arrayOfRows = $('tbody').find('tr').toArray();
-    console.log($arrayOfRows);
+    let rows = $('tbody').find('tr').toArray();
+    console.log(rows[0]);
 
-    $('th').on('click', function() {
+    // click event on the column header
+    $('tr').on('click', 'th', function() {
         console.log('clicked on the "' + $(this).data('sort') + '" header');
-    });
+
+        $headerName = $(this).data('sort');
+        // console.log($headerName);
+        // console.log($(this).val());
+
+        rows.sort( function(a, b) {
+            a = $(a).find('td').eq(0).text();
+            b = $(b).find('td').eq(0).text();
+
+            console.log('a', a);
+            console.log('b', b);
+
+            if (a < b) {
+                return -1;
+            } else {
+                return a > b ? 1 : 0;
+            }
+        });
+        console.log(rows[0]);
+        $('tbody').append(rows);
+
+    })
 });
