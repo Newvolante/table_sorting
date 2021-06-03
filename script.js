43eb9848a6ac7801642ca5993397b4e4d4c4b5c8
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-03 15:31:19 +0200

    added logic to handle various clicks on a column header

--- script.js
+++ script.js
@@ -3,31 +3,26 @@ $(function() {
 
     // an array of the rows present in the table
     let rows = $('tbody').find('tr').toArray();
-    console.log(rows[0]);
 
     // click event on the column header
     $('tr').on('click', 'th', function() {
 
-        $(this).siblings().removeClass('sorted');
-
-        if($(this).hasClass('sorted')) {
-            rows.reverse();
-        }
-        
-        // calling sorting function from sortObject.js
-        if(!$(this).hasClass('sorted')) {
+        if($(this).is('.ascending') || $(this).is('.descending')) {
+            $(this).toggleClass('ascending descending');
+            $('tbody').append(rows.reverse());
+        } else {
+            $(this).addClass('ascending');
+                        
             $headerName = $(this).data('sort');     // function name to call
             $columnIndex = $('th').index(this);     // column number
-
+            
             rows.sort(function(a, b) {
                 a = $(a).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
                 b = $(b).find('td').eq($columnIndex).text();   // using jQ selector to use jQ methods
                 
                 return sortObject[$headerName](a, b);
             });        
-            
-            $(this).addClass('sorted');
-        }        
-        $('tbody').append(rows);    // appends the sorted arrays of rows to the table body
-    })
-});
+            $('tbody').append(rows);    // appends the sorted arrays of rows to the table body
+        }
+        })
+    });
